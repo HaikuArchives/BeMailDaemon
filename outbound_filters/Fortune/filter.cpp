6de5390920751d45a65dbf26d1d541a234089295
brunoga <>
2001-09-26 00:23:10 +0000

    Now adds a line break (\n) before writting the fortune.

--- outbound_filters/Fortune/filter.cpp
+++ outbound_filters/Fortune/filter.cpp
@@ -23,7 +23,6 @@ class FortuneFilter: public MailFilter
 FortuneFilter::FortuneFilter(BMessage* msg)
 : MailFilter(msg), enabled(msg->FindBool("enabled"))
 {
-	printf("Constructor called.\n");
 }
 
 status_t FortuneFilter::InitCheck(BString* err){ return B_OK; }
@@ -31,22 +30,21 @@ status_t FortuneFilter::InitCheck(BString* err){ return B_OK; }
 MDStatus FortuneFilter::ProcessMailMessage
 (BPositionIO** io, BEntry* io_entry, BMessage* headers, BPath* , BString*)
 {
-	printf ("Start your engines!\n");
 	FILE * fd;
 	char buffer[768];
 	
 	fd = popen("/bin/fortune", "r");
 	if (fd) {
-		printf("Looking good...\n");
 		(*io)->Seek(0, SEEK_END);
-
+		
+		(*io)->Write("\n", strlen("\n"));	
+	
 		while (fgets(buffer, 768, fd)) {
 			(*io)->Write(buffer, strlen(buffer));
 		}
 	} else {
-		printf("Damnit!...\n");
+		printf("Damnit!\n");
 	}
-	printf("Returning...\n");		
 	return MD_OK;
 }
 
