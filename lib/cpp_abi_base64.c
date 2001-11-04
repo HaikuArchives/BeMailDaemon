0dc8ebe0c56b5f283d5b451d6464a9f7f69ad9bf
nwhitehorn <>
2001-11-04 18:14:10 +0000

    Now has cpp_abi_base64 as C. Why not?

--- lib/cpp_abi_base64.c
+++ lib/cpp_abi_base64.c
@@ -9,14 +9,14 @@
 #define decode_base64__local_abi decode_base64__FPcT0xb
 #endif
 
-extern "C" _EXPORT ssize_t encode_base64__local_abi(char *out, char *in, off_t length);
-extern "C" _EXPORT ssize_t decode_base64__local_abi(char *out, char *in, off_t length, bool replace_cr);
+ssize_t encode_base64__local_abi(char *out, char *in, off_t length);
+ssize_t decode_base64__local_abi(char *out, char *in, off_t length, char replace_cr);
 
-ssize_t encode_base64__local_abi(char *out, char *in, off_t length) {
+_EXPORT ssize_t encode_base64__local_abi(char *out, char *in, off_t length) {
 	return encode_base64(out,in,length);
 }
 
-ssize_t decode_base64__local_abi(char *out, char *in, off_t length, bool /*replace_cr*/) {
+_EXPORT ssize_t decode_base64__local_abi(char *out, char *in, off_t length, char /*replace_cr*/) {
 	return decode_base64(out,in,length);
 }
 
