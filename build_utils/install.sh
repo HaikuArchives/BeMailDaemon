79b643e3b1d74cf2428b573aa63affe0dd8dadf9
nwhitehorn <>
2001-11-06 22:35:13 +0000

    New and improved for r2!

--- build_utils/install.sh
+++ build_utils/install.sh
@@ -2,16 +2,17 @@
 
 cd "$(dirname "$0")"
 
-RETURN=`alert "There can only be ONE version of mail_daemon on the system at one time (see the enclosed README file for why).  Choose 'Backup' if you wish to keep your old mail_daemon and E-mail preferences app, and this is the first time you're installing the Mail Daemon Replacement.  Otherwise, you should choose 'Purge' to clear the other versions from your system." "Purge" "Backup" "Don't do anything!"`
+RETURN=`alert "There can only be ONE version of mail_daemon on the system at one time (see the enclosed README file for why).  Choose 'Backup' if you wish to keep your old mail_daemon, E-mail preferences app, and BeMail, and this is the first time you're installing the Mail Daemon Replacement.  Otherwise, you should choose 'Purge' to clear the other versions from your system." "Purge" "Backup" "Don't do anything!"`
 
 if [[ $RETURN = "Purge" ]]
 then
-	query -a 'BEOS:APP_SIG == "application/x-vnd.Be-POST" || BEOS:APP_SIG == "application/x-vnd.Be-mprf"' \
+	# note: we don't remove libmail.so, because it doesn't matter here, and there may be symlinks and things
+	query -a 'BEOS:APP_SIG == "application/x-vnd.Be-POST" || BEOS:APP_SIG == "application/x-vnd.Be-mprf" || 'BEOS:APP_SIG == "application/x-vnd.Be-MAIL"' \
 	| grep -v "`/bin/pwd`" \
 	| xargs rm
 elif [[ $RETURN = "Backup" ]]
 then 
-	query -a 'BEOS:APP_SIG == "application/x-vnd.Be-POST" || BEOS:APP_SIG == "application/x-vnd.Be-mprf" || name == libmail.so' \
+	query -a 'BEOS:APP_SIG == "application/x-vnd.Be-POST" || BEOS:APP_SIG == "application/x-vnd.Be-mprf" || 'BEOS:APP_SIG == "application/x-vnd.Be-MAIL" || name == libmail.so' \
 	| grep -v "`/bin/pwd`" \
 	| xargs zip -ym /boot/home/maildaemon.zip
 else 
@@ -30,9 +31,10 @@ copyattr -d -m bin/libmail.so /boot/system/lib/libmail.so
 
 copyattr -d -m -r bin/addons/* ~/config/add-ons/mail_daemon
 
-rm -f /boot/beos/system/servers/mail_daemon /boot/beos/preferences/E-mail
+rm -f /boot/beos/system/servers/mail_daemon /boot/beos/preferences/E-mail /boot/beos/apps/BeMail
 copyattr -d -m bin/mail_daemon /boot/beos/system/servers/mail_daemon
 copyattr -d -m bin/E-mail /boot/beos/preferences/E-mail
+copyattr -d -m bin/BeMail /boot/beos/apps/BeMail
 sleep 1
 /boot/beos/system/Deskbar &
 sleep 1
