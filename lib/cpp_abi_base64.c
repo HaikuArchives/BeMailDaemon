79afec4a55a0ba47f4465ff941334a6e0a4309bd
nwhitehorn <>
2002-01-11 22:07:22 +0000

    Modified ABI hack so it makes more sense.

--- lib/cpp_abi_base64.c
+++ lib/cpp_abi_base64.c
@@ -1,13 +1,15 @@
 #include <mail_encoding.h>
 
-#if __POWERPC__
-#define encode_base64__local_abi encode_base64__FPcPcx
-#define decode_base64__local_abi decode_base64__FPcPcxb
+#if __MWERKS__
+	#define encode_base64__local_abi encode_base64__FPcPcx
+	#define decode_base64__local_abi decode_base64__FPcPcxb
+#elif __GNUC__ <= 2
+	#define encode_base64__local_abi encode_base64__FPcT0x
+	#define decode_base64__local_abi decode_base64__FPcT0xb
 #else
-//--------Note, this is for GCC 2 only
-#define encode_base64__local_abi encode_base64__FPcT0x
-#define decode_base64__local_abi decode_base64__FPcT0xb
+	#error "We don't seem to have a C++ ABI hack for your compiler. Please add one."
 #endif
+
 
 ssize_t encode_base64__local_abi(char *out, char *in, off_t length);
 ssize_t decode_base64__local_abi(char *out, char *in, off_t length, char);
