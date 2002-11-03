1bcea1faadc603a4846f035deb115eec45052edd
agmsmith <>
2002-11-03 02:21:02 +0000

    Never mind, just use the SourceForge version numbers.  Ugh.

--- inbound_filters/AGMSBayesianSpamFilter/AGMSBayesianSpamFilter.cpp
+++ inbound_filters/AGMSBayesianSpamFilter/AGMSBayesianSpamFilter.cpp
@@ -11,7 +11,6 @@
  * Public Domain 2002, by Alexander G. M. Smith, no warranty.
  *
  * $Log$
- *
  * Revision 1.8  2002/10/21 16:12:09  agmsmith
  * Added option for spam if no words found, use new method of saving
  * the attribute which avoids hacking the rest of the mail system.
