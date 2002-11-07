4ae9ccee406ba2bd1fda4391e6c9a8de02c43f15
agmsmith <>
2002-11-07 21:04:00 +0000

    Check for spaces in path name (was causing installations to fail), and also install the AGMSBayesianSpam database.

--- build_utils/install.sh
+++ build_utils/install.sh
@@ -2,6 +2,11 @@
 
 cd "$(dirname "$0")"
 
+if pwd | grep " ";
+  then alert "Please install from a directory with no spaces in the path name, the current one (`pwd`) has spaces.  Just move all the files to somewhere that doesn't have spaces, like /boot/home/mydirectory.  If there are spaces, the install script malfunctions.  Sorry.";
+  exit 1;
+fi
+
 RETURN=`alert "There can only be ONE version of mail_daemon on the system at one time (see the enclosed README file for why).  Choose 'Backup' if you wish to keep your old mail_daemon, E-mail preferences app, and BeMail, and this is the first time you're installing the Mail Daemon Replacement.  Otherwise, you should choose 'Purge' to clear the other versions from your system." "Purge" "Backup" "Don't do anything!"`
 
 if [[ $RETURN = Purge ]]
@@ -30,7 +35,17 @@ copyattr -d -m -r bin/addons/* ~/config/add-ons/mail_daemon
 copyattr -d -m bin/mail_daemon /system/servers/mail_daemon
 copyattr -d -m bin/E-mail /boot/beos/preferences/E-mail
 copyattr -d -m bin/BeMail /boot/beos/apps/BeMail
+
+# Set up the spam classifier server.
 copyattr -d -m bin/AGMSBayesianSpamServer ~/config/bin/AGMSBayesianSpamServer
+mkdir -p ~/config/settings/AGMSBayesianSpam
+if test -e "${HOME}/config/settings/AGMSBayesianSpam/AGMSBayesianSpam Database";
+  then
+    alert "The spam database file (${HOME}/config/settings/AGMSBayesianSpam/AGMSBayesianSpam Database) already exists.  You can manually replace it with the SampleSpamDatabase if you wish to.  However, if you have customized your spam words (recommended so that it recognizes what YOUR genuine mail and spam look like) then you probably want to keep using your existing database.";
+  else
+    cp SampleSpamDatabase "${HOME}/config/settings/AGMSBayesianSpam/AGMSBayesianSpam Database";
+    rm SampleSpamDatabase;
+fi 
 
 sleep 1
 /system/Deskbar &
