db3b73a1ccd34baeecc45fb5a83ab978ff979f99
agmsmith <>
2003-01-30 23:26:07 +0000

    Starting to add a simplistic internationalization system for "Koki", who wants to use BeMail in Japanese.  It should later be replaced by a more comprehensive system, but at least this one will mark out the spots where translated text is needed.

---
+++ include/support/MDRLanguage.h
@@ -0,0 +1,18 @@
+#ifndef ZOIDBERG_MDR_LANGUAGE_H
+#define ZOIDBERG_MDR_LANGUAGE_H
+/* Mail Daemon Replacement interim International Language Macros.
+**
+** Copyright 2003 Dr. Zoidberg Enterprises.  All rights reserved.
+**
+** The input for the language macro system is the MDR_LANGUAGE define which is
+** set by the makefile (you compile a version for the desired language, it
+** can't change at run-time using this temporary internationalization system).
+** MDR_LANGUAGE was set to 0 for English-UK, 1 for Japanese, and other numbers
+** for other languages.  If it's not present, we use English-UK.
+**
+** $Log$
+*/
+
+
+#endif	/* ZOIDBERG_MDR_LANGUAGE_H */
+
