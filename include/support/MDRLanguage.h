b10e867bfc7872c531e0b039ff6d7d7edd4e4a2b
agmsmith <>
2003-02-05 22:43:17 +0000

    Default language (judging by the use of "color" in the bemail preferences) changed to be English-USA.

--- include/support/MDRLanguage.h
+++ include/support/MDRLanguage.h
@@ -8,10 +8,13 @@
 ** The input for the language macro system is the MDR_DIALECT define which is
 ** set by the makefile (you compile a version for the desired language, it
 ** can't change at run-time using this temporary internationalization system).
-** MDR_DIALECT was set to 0 for English-UK, 1 for Japanese, and other numbers
-** for other language dialects.  If it's not present, we use English-UK.
+** MDR_DIALECT was set to 0 for English-USA, 1 for Japanese, and other numbers
+** for other language dialects.  If it's not present, we use English-USA.
 **
 ** $Log$
+** Revision 1.2  2003/01/30 23:52:12  agmsmith
+** Initial simple language system macros done.
+**
 ** Revision 1.1  2003/01/30 23:26:07  agmsmith
 ** Starting to add a simplistic internationalization system for "Koki",
 ** who wants to use BeMail in Japanese.  It should later be replaced by
@@ -19,19 +22,19 @@
 ** spots where translated text is needed.
 */
 
-#define MDR_DIALECT_ENGLISH_UK 0
+#define MDR_DIALECT_ENGLISH_USA 0
 #define MDR_DIALECT_JAPANESE 1
 
 #ifndef MDR_DIALECT
-	#define MDR_DIALECT MDR_DIALECT_ENGLISH_UK
+	#define MDR_DIALECT MDR_DIALECT_ENGLISH_USA
 #endif
 
-#if (MDR_DIALECT == MDR_DIALECT_ENGLISH_UK)
-	#define MDR_DIALECT_CHOICE(EnglishUK,Japanese) EnglishUK
-	#define MDR_COUNTRY "United Kingdom"
+#if (MDR_DIALECT == MDR_DIALECT_ENGLISH_USA)
+	#define MDR_DIALECT_CHOICE(EnglishUSA,Japanese) EnglishUSA
+	#define MDR_COUNTRY "United States of America"
 	#define MDR_LANGUAGE "English"
 #elif (MDR_DIALECT == MDR_DIALECT_JAPANESE)
-	#define MDR_DIALECT_CHOICE(EnglishUK,Japanese) Japanese
+	#define MDR_DIALECT_CHOICE(EnglishUSA,Japanese) Japanese
 	#define MDR_COUNTRY "Japan"
 	#define MDR_LANGUAGE "Japanese"
 #else
