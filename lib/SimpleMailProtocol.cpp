cf0fdb97ae3f9ea686e35c24e97cdfe14462672a
axeld <>
2003-07-01 21:15:35 +0000

    Removed unused code. Applied the OpenTracker style guide - if someone don't like that I do that, please shout out loud :-)

--- lib/SimpleMailProtocol.cpp
+++ lib/SimpleMailProtocol.cpp
@@ -42,101 +42,102 @@ class SinglePassTermination : public Mail::ChainCallback {
 SimpleProtocol::SimpleProtocol(BMessage *settings, Mail::ChainRunner *run) :
 	Mail::Protocol(settings,run),
 	error(B_OK),
-	last_message(-1) {
-		runner->RegisterProcessCallback(new SinglePassTermination(runner));
-	}
+	last_message(-1)
+{
+	runner->RegisterProcessCallback(new SinglePassTermination(runner));
+}
 
-status_t SimpleProtocol::Init() {
-		
-	error = Open(settings->FindString("server"),settings->FindInt32("port"),settings->FindInt32("flavor"));
+
+status_t
+SimpleProtocol::Init()
+{
+	error = Open(settings->FindString("server"), settings->FindInt32("port"),
+				settings->FindInt32("flavor"));
 	if (error < B_OK) {
 		runner->Stop();
 		return error;
 	}
-	
+
 	const char *password = settings->FindString("password");
-	char *passwd = get_passwd(settings,"cpasswd");
+	char *passwd = get_passwd(settings, "cpasswd");
 	if (passwd)
 		password = passwd;
 
-	error = Login(settings->FindString("username"),password,settings->FindInt32("auth_method"));
+	error = Login(settings->FindString("username"), password, settings->FindInt32("auth_method"));
 	delete passwd;
-	
+
 	if (error < B_OK) {
 		runner->Stop();
 		return error;
 	}
-	
+
 	if (settings->FindBool("login_and_do_nothing_else_of_any_importance"))
 		return error;
-	
+
 	error = UniqueIDs();
 	if (error < B_OK) {
 		runner->Stop();
 		return error;
 	}
-		
-	//if (settings->FindBool("leave_mail_on_server")) {
-		size_t	maildrop_size = 0;
-		int32	num_messages;
-		
-		StringList to_dl;
-		manifest->NotHere(*unique_ids,&to_dl);
-		
-		num_messages = to_dl.CountItems();
-		if (num_messages == 0) {
-			runner->Stop();
-			return error;
-		}
-		
-		for (int32 i = 0; i < to_dl.CountItems(); i++)
-			maildrop_size += MessageSize(unique_ids->IndexOf(to_dl[i]));
-		
-		runner->GetMessages(&to_dl,maildrop_size);
-	/*} else {
-		if (unique_ids->CountItems() == 0) {
-			runner->Stop();
-			return error;
-		}
-			
-		runner->GetMessages(unique_ids,MailDropSize());
-	}*/
-	
+
+	size_t	maildrop_size = 0;
+	int32	num_messages;
+
+	StringList to_dl;
+	manifest->NotHere(*unique_ids, &to_dl);
+
+	num_messages = to_dl.CountItems();
+	if (num_messages == 0) {
+		runner->Stop();
+		return error;
+	}
+
+	for (int32 i = 0; i < to_dl.CountItems(); i++)
+		maildrop_size += MessageSize(unique_ids->IndexOf(to_dl[i]));
+
+	runner->GetMessages(&to_dl, maildrop_size);
+
 	return error;
 }
 
-SimpleProtocol::~SimpleProtocol() {
+
+SimpleProtocol::~SimpleProtocol()
+{
 }
 	
 
-status_t SimpleProtocol::GetMessage(
-	const char* uid,
-	BPositionIO** out_file, BMessage* out_headers,
-	BPath* out_folder_location
-) {
+status_t
+SimpleProtocol::GetMessage(const char *uid, BPositionIO **out_file, BMessage *out_headers,
+	BPath *out_folder_location)
+{
 	int32 to_retrieve = unique_ids->IndexOf(uid);
 	if (to_retrieve < 0)
 		return B_NAME_NOT_FOUND;
-	
-	
+
 	out_headers->AddInt32("SIZE",MessageSize(to_retrieve));
 	*out_file = new Zoidberg::Mail::MessageIO(this,*out_file,to_retrieve);
-	
+
 	if (out_folder_location != NULL)
 		out_folder_location->SetTo("");
-	
+
 	return B_OK;
 }
 
-status_t SimpleProtocol::DeleteMessage(const char* uid) {
-	#if DEBUG
-	 printf("ID is %d\n", (int)unique_ids->IndexOf(uid)); // What should we use for int32 instead of %d?
-	#endif
+
+status_t
+SimpleProtocol::DeleteMessage(const char *uid)
+{
+#if DEBUG
+	printf("ID is %d\n", (int)unique_ids->IndexOf(uid)); // What should we use for int32 instead of %d?
+#endif
 	Delete(unique_ids->IndexOf(uid));
 	return B_OK;
 }
 
-status_t SimpleProtocol::InitCheck(BString* /*out_message*/) {
+
+status_t
+SimpleProtocol::InitCheck(BString* /*out_message*/)
+{
 	return error;
 }
 
